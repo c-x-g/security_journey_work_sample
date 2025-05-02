38d9bc58ae9077e8beb5da23221c3c793eda3185
Carl Guan <xicong.guan@gmail.com>
2025-05-01 21:48:52 -0400

    refine login logic in insecure app to lookup users from backend instead of frontend

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,10 +13,19 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/user", (req, res) => {
+  const { name } = req.query;
+  if (name && name in db["Users"]) {
+    res.json(db["Users"][name]);
+  } else {
+    res.json({ id: -1 });
+  }
+});
+
 app.get("/documents", (req, res) => {
   const { name } = req.query;
-  if (name && name in db) {
-    res.json(db[name]);
+  if (name && name in db["Documents"]) {
+    res.json(db["Documents"][name]);
   } else {
     res.status(400).json({ error: "please provide a valid username" });
   }
