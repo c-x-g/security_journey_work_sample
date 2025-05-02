7b3abf6410a01512c147d19d2b45f0ff8f59968f
Carl Guan <xicong.guan@gmail.com>
2025-05-02 11:35:15 -0400

    make master conform to secure-changes branch

--- public/script.js
+++ public/script.js
@@ -2,12 +2,14 @@ const app = document.getElementById("app");
 const url = window.location.origin;
 
 let username = "";
+let userId = -1;
 let hasAppended = false;
 let loginText = "Please login";
 
 function render() {
   username = localStorage.getItem("username");
-  if (username && username !== "invalid") {
+  userId = localStorage.getItem("userId");
+  if (username && userId != -1) {
     app.innerHTML = `
           <h1>Welcome, ${username}!</h1>
           <button onclick="getDocument()">View Personal Information</button>
@@ -30,11 +32,8 @@ function login() {
     fetch(`${url}/user?name=${name}`).then((res) => {
       res.json().then((res) => {
         const { id } = res;
-        if (id !== -1) {
-          localStorage.setItem("username", name);
-        } else {
-          localStorage.setItem("username", "invalid");
-        }
+        localStorage.setItem("username", name);
+        localStorage.setItem("userId", id);
         render();
       });
     });
